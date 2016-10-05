14cf1aa0dbbaaf53798da31e0290bb749af9726e
Mher Margaryan <margaryan.mher.28@gmail.com>
2016-10-05 16:33:04 +0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+	$(window).scroll(function(){
+		var y = $(this).scrollTop();
+
+		if (y>=200) {
+			$('#laptop').addClass('animate');
+			$('#mobile').addClass('animate');
+		}
+
+		if (y<=200) {
+			$('#laptop').removeClass('animate');
+			$('#mobile').removeClass('animate');
+		}
+	});
+})
